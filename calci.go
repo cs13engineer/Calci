8a8bddb780851e3e870a33b1add45679dee4280a
cs13engineer <75882819+cs13engineer@users.noreply.github.com>
2021-08-13 13:35:06 +0530

    package template for calculator application

---
+++ calci.go
@@ -0,0 +1,42 @@
+package calci
+
+//will add integers
+func addInt(i int, j int)int{
+	return(i+j)
+}
+
+//will add float32
+func addFloat(i Float32, j Float32)Float32{
+	return(i+j)
+}
+
+//will subtract integers
+func subInt(i int, j int)int{
+	return(i-j)
+}
+
+//will subtract float32
+func subFloat(i Float32, j Float32)Float32{
+	return(i-j)
+}
+
+//will multiply integers
+func subMul(i int, j int)int{
+	return(i*j)
+}
+
+//will multiply float32
+func subMul(i Float32, j Float32)Float32{
+	return(i*j)
+}
+
+//will divide integers
+func subDiv(i int, j int)int{
+	return(i/j)
+}
+
+//will divide the Float32 type
+func subDiv(i Float32, j Float32)Float32{
+	return(i/j)
+}
+
